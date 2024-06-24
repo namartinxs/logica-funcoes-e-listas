99ba3ed79a309078b43945a4cf6c0a113391cc29
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-06-24 00:21:55 -0300

    função que retorna o dobro de um número

---
+++ desafiodobro.js
@@ -0,0 +1,8 @@
+let askNumero = prompt('Descubra o dobro de um número');
+
+function dobradora(numero){
+    return numero * 2;
+}
+
+let resultado = dobradora(askNumero);
+console.log('O resultado desta multiplicação é ' + resultado)
