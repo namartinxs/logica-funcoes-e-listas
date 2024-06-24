4a26dc70faad8e0e6609bffca467366acde341c3
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-06-23 21:23:33 -0300

    funções e querrySelector

---
+++ app.js
@@ -0,0 +1,11 @@
+//           p/ o html
+let titulo = document.querySelector('h1');
+titulo.innerHTML ='Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+//funcao, trecho de codigo com funçao, responsabilidade
+function verificarChute() {
+    console.log('O botao foi clicado')
+}
