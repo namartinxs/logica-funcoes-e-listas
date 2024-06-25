dfa300ed0ac9919f779acd31b0b64a0a1e777c98
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-06-25 17:22:39 -0300

    calculo do perimetro e area em metros quadrados

---
+++ perimetro.js
@@ -0,0 +1,12 @@
+function calcArPe(altura,largura){
+
+    let areaM = altura * largura;
+    let perM = 2 * (altura+largura)
+    console.log(`Área da sala: ${area} metros quadrados`);
+    console.log(`Perímetro da sala: ${perimetro} metros`);
+}
+
+let altura = 5;
+let largura = 2;
+
+calcArPe(altura,largura);
