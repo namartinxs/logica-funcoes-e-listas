eb6ab811aa7a98afd7bfb8556c5a03f733356cc8
Nathalia Martins <133926013+namartinxs@users.noreply.github.com>
2024-06-25 19:04:40 -0300

    exercicio com listas

---
+++ desafio4.js
@@ -0,0 +1,8 @@
+let listaGenerica = []
+let linguagensDeProgramacao = ['JS','C','C++','Kotlin','Python'];
+linguagensDeProgramacao.push('Java','Ruby','GoLang');
+
+let nome = ['Rita','Joao','Teresa'];
+console.log(nome[0]);
+console.log(nome[1]);
+console.log(nome[nome.length-1]);
